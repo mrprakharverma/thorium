e0a64145d0248f13d6d64feeefc34fb07d2a8388
Prakhar Verma <prakharvermashivam2015@gmail.com>
2022-02-23 00:30:28 +0530

    second commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,4 +10,118 @@ router.get('/students/:name', function(req, res) {
 
 
 
+
+router.get('/movies',function(req,res){
+    const mymovie = ['Pushpa','Aiyaary','Hridayam','The Nun']
+     res.send(mymovie);
+})
+
+router.get('/movies/:index',function(req,res){
+
+const mymovie = ['Pushpa','Aiyaary','Hridayam','The Nun']
+let prakhar =req.params.index;
+if(prakhar>mymovie.length-1){
+    res.send('Use valid index')
+}
+else if(prakhar<mymovie.length){
+
+    let y = mymovie[prakhar]
+    res.send(y)
+}
+
+
+})
+ 
+
+
+
+router.get('/films', function(req,res){
+
+const film = [
+
+    {
+        "id":1, "name":"Pushpa"
+    },
+   
+    {
+        "id":2, "name":"Aiyaary"   
+     },
+
+     {
+         "id":3, "name":"Hridayam"
+
+
+     },
+
+     {
+        
+        "id":4 , "name":"The Nun"
+
+     }
+
+
+]
+res.send(film)
+
+})
+
+
+
+
+
+
+router.get('/films/:filmsid', function(req,res){
+      
+
+    let fi=req.params.filmsid;
+    const film = [
+    
+        {
+            "id":1, "name":"Pushpa"
+        },
+       
+        {
+            "id":2, "name":"Aiyaary"   
+         },
+    
+         {
+             "id":3, "name":"Hridayam"
+    
+    
+         },
+    
+         {
+            
+            "id":4 , "name":"The Nun"
+    
+         }
+    
+    
+    ]
+
+    if(fi>film.length){
+
+        res.send("invalid id")
+    }
+    else {
+
+        for(let i=0;i<film.length;i++){
+            if(film[i].id==fi){
+
+                res.send(film[i])
+            }
+
+        }
+    }
+   
+   
+   
+   
+    res.send(film)
+    
+    })
+
+
+
+
 module.exports = router;
