9ddde328137c54f08c4629ff7cdd7c4ed210bd13
Prakhar Verma <prakharvermashivam2015@gmail.com>
2022-02-24 01:31:16 +0530

    fourth commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,85 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+let Player = [ 
+    {
+          
+         "name": "Pritesh",
+         "dob": "10/08/1990",
+         "gender":"male",
+         "city": "Banglore",
+         "sports": [ " Cricket"],
+         "bookings": [
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+            {
+                "bookingNumber":1,
+                "sportsId":"",
+                "centreId":"",
+                "type":"private",
+                "slot": "11111",
+                "bookedOn":"03/06/2022",
+                "bookedFor":" 10/06/2022"
+            },
+            {
+                "bookingNumber":2,
+                "sportsId":"",
+                "centreId":"",
+                "type":"private",
+                "slot": "22222",
+                "bookedOn":"15/07/2021",
+                "bookedFor":" 01/12/2021"
+            }
 
+         ]
+  },
+ 
+ 
+  {
+    "name": "Bharat",
+    "dob": "09/06/1992",
+    "gender":"male",
+    "city": "Hariyana",
+    "sports": [ " Football"],
+    "bookings": [   ]
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+ },
 
+  {
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+    "name": "Pritesh",
+    "dob": "19/12/1994",
+    "gender":"male",
+    "city": "Punjab",
+    "sports": [ " Gaziabad"],
+    "bookings": [  ]
+}
 
+]
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+let a = Player.length;
 
+// Part 1 ==> Add new player.
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+router.post('/player',function(req,res){
 
+let element = req.body.nplayer.name;
+let element1 = req.body.nplayer
+for (let i=0;i<a;i++){
+if(element === Player[i].name ){
+    console.log(element)
+    res.send("This Player is already exists")
+    
+   
+}
+else if (i === a-1){
 
+    Player.push(element1)
+    console.log(element1)
+    res.send({data :Player , status : true})
+   
+}
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
+}
 })
 
 module.exports = router;
